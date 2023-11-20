fc0c7be7989e9b7f71678d6c1b60af875f1e272e
Your Name <you@example.com>
2023-11-20 18:04:00 +0300

    refactoring

--- homepage/index.js
+++ homepage/index.js
@@ -1,8 +1,8 @@
 function add_items() {
-    let table = document.getElementsByName("client_items")[0];
+    let table = document.getElementsById("client_items")[0];
     let row = table.insertRow(-1);
     let c1 = row.insertCell(0);
-    console.log("fff");
+    console.log("fff");S
     let c2 = row.insertCell(1);
     let c3 = row.insertCell(2);
     let c4 = row.insertCell(3);
@@ -16,7 +16,7 @@ function add_items() {
 function filter_items() {
     let word = document.getElementById("filter_parameter").value;
     console.log(word);
-    let tables = document.getElementsByName("cmp_data");
+    let tables = document.getElementsByClassName("table");
     let result = "";
     for (let table of tables){
         for (let row of table.getElementsByTagName('tr')){
@@ -34,3 +34,19 @@ function filter_items() {
         document.getElementById("filter_results").innerText = result;
     }
 }
+
+function blink() {
+    let text_filter = document.querySelector('#filter_results');
+    if (text_filter === "try the new filter!")
+    {
+        if (text_filter.style.visibility == 'hidden')
+        {
+            text_filter.style.visibility = 'visible';
+        }
+        else
+        {
+            text_filter.style.visibility = 'hidden';
+        }
+    }
+}
+window.setInterval(blink, 500);
